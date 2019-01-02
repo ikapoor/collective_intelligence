3e1277dfa66137a8272fe352619cb5d63de813bd
Ishan Kapoor <ikapoor@uwaterloo.ca>
2019-01-01 23:22:18 -0500

    adding searchengine.py

--- collective intelligence/search_engine/searchengine.py
+++ collective intelligence/search_engine/searchengine.py
@@ -1,3 +1,10 @@
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
+
+# Create a list of words to ignore
+ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
+
 class crawler:
     # Initialize the crawler with the name of database
     def __init__(self,dbname):
@@ -40,8 +47,29 @@ class crawler:
     # first search to the given depth, indexing pages
     # as we go
     def crawl(self, pages, depth=2):
-        pass
+        for i in range(depth):
+            newpages = set()
+            for page in pages:
+                try:
+                    c = urllib2.urlopen(page)
+                except:
+                    print("Could not open %s", page)
+                    continue
+                soup = BeautifulSoup(c.read())
+                self.addtoindex(page, soup)
 
+                links = soup('a')
+                for link in links:
+                    if ('href' in dict(link.attrs)):
+                        url = urljoin(page, link['href'])
+                        if (url.find("'") != -1): continue
+                        url = url.split("#")[0]
+                        if (url[0:4] == 'http' and not self.isindexed(url)):
+                            newpages.add(url)
+                        linkText = self.gettextonly(link)
+                        self.addlinkref(page, url, linkText)
+                self.dbcommit()
+                pages = newpages
 
     # Create the database tables
     def createindextables(self):
