4dc2b5d1a207318fc73be40caff700301e9f550e
Ishan Kapoor <ikapoor@uwaterloo.ca>
2019-01-02 00:30:44 -0500

    added database api, using sqlite

--- collective intelligence/search_engine/searchengine.py
+++ collective intelligence/search_engine/searchengine.py
@@ -1,40 +1,107 @@
-import urllib2
-from BeautifulSoup import *
-from urlparse import urljoin
+import urllib.request
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from sqlite3 import dbapi2 as sqlite
+import re
 
 # Create a list of words to ignore
-ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
+ignore_words = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 
 class crawler:
+
+
     # Initialize the crawler with the name of database
     def __init__(self,dbname):
-        pass
+        self.con = sqlite.connect(dbname)
 
     def __del__(self):
-        pass
+        self.con.close()
 
     def dbcommit(self):
-        pass
+        self.con.commit()
+
+    def createindextables(self):
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid,wordid,location)')
+        self.con.execute('create table link(fromid integer,toid integer)')
+        self.con.execute('create table linkwords(wordid,linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
+        self.dbcommit()
+
+
     # Auxilliary function for getting an entry id and adding
     # it if it's not present
     def getentryid(self,table,field,value,createnew=True):
-        return None
+        cur = self.con.execute(
+        " select rowid from %s where %s = '%s'" %(table, field, value))
+        res = cur.fetchone()
+        if res == None:
+            cur = self.con.execute(
+            "insert into %s (%s) values ('%s')" %( table, field, value))
+            return cur.lastrowid
+        else:
+            return res[0]
+
+
 
     # Index an individual page
     def addtoindex(self,url,soup):
+
+        if self.isindexed(url): return
         print ('Indexing %s' % url)
+
+
+        #getting individual words
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
+
+        # get the URL id
+        url_id = self.getentryid('urllist', 'url' , url)
+
+        # link each of the words to this url
+        for i in range(len(words)):
+            word = words[i]
+            if (word in ignore_words): continue
+            word_id = self.getentryid('wordlist' , 'word', word)
+            self.con.execute("insert into wordlocation(urlid,wordid,location) \
+            values (%d,%d,%d)" % (url_id, word_id, i))
+
+
+
         # Extract the text from an HTML page (no tags)
 
-    def gettextonly(self,soup):
-        return None
+    def gettextonly(self, soup):
+        v = soup.string
+        if v == None:
+            c = soup.contents
+            resulttext = ''
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+            return resulttext
+        else:
+            return v.strip()
 
 
     # Separate the words by any non-whitespace character
-    def separatewords(self,text):
-        return None
+    def separatewords(self, text):
+        splitter = re.compile('\\W*')
+        return [s.lower() for s in splitter.split(text) if s != '']
 
-        # Return true if this url is already indexed
+    # Return true if this url is already indexed
     def isindexed(self, url):
+        u = self.con.execute \
+            ("select rowid from urllist where url='%s'" % url).fetchone()
+        if u != None:
+            #Check if it has actually been crawled
+            v = self.con.execute(
+                'select * from wordlocation where urlid=%d' % u[0]).fetchone()
+            if v != None: return True
         return False
 
 
@@ -51,7 +118,7 @@ class crawler:
             newpages = set()
             for page in pages:
                 try:
-                    c = urllib2.urlopen(page)
+                    c = urllib.request.urlopen(page)
                 except:
                     print("Could not open %s", page)
                     continue
@@ -71,6 +138,3 @@ class crawler:
                 self.dbcommit()
                 pages = newpages
 
-    # Create the database tables
-    def createindextables(self):
-        pass
